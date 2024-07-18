76b9f05e08b3059f97b59e6f51fc281f79656560
Omooba442 <74331985+Omooba442@users.noreply.github.com>
2024-07-18 10:26:50 +0100

    new php form

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="main.css">
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            <!--contact form-->
+            <section class="contact">
+                <div class="container-lg">
+                    <div class="text-center">
+                        Get In touch 
+                    </div>
+                    <p class="para"> Suscipit incidunt quae cumque nulla qui nam architecto maiores optio, impedit pariatur praesentium nemo in totam numquam magnam quia repellat temporibus expedita!</p>
+
+                </div>
+                <div class="row justify-content-centre">
+                <div class="col-lg-6">
+                    <form>
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" class="email" placeholder="Please enter your email">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" placeholder="Enter your password" class="form-control">
+                        <label for="name" class="form-label">Fullname </label>
+                        <input type="text" placeholder="Enter your fullname" class="form-control">
+                        <label for="subject"> Enter your subject</label>
+                        <select class="form-select form-select" id="subject">
+                            <option value="pricing" >option 1</option>
+                            <option value="colours"> option 2</option>
+                            <option value="redmi" selected>Redmi</option>
+                        </select> 
+                  
+                        
+                    </form>
+                </div>
+                </div>
+
+            </section>
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
---
+++ main.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Facebook</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="stylesheet" href="main.css">
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <br>
+                <br>
+                <br>
+                <br>
+                <div class="first">
+                    <P class="text-blue mb-0">FACEBOOK</P>
+                    <p class="text-black">FACEBOOK helps you to connect and <br> shre love alwways</b> </p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="second">
+                    <div class="card">
+                        <div class="card-body">
+                            <label for="" class="form-label">Email</label>
+                            <input type="email" class="form-control mt-3" placeholder="please enter your email" maxlength="30">
+                            <label for="" class="form-label">Username</label>
+                            <input type="text" class="form-control mt-3" placeholder="please enter your username" maxlength="7">
+                            <label for="" class="form-label">Phone Number</label>
+                            <input type="number" class="form-control mt-3" placeholder="please enter your phone "  maxlength="11">
+                        </div>
+                        <select class="form-select">
+                            <option selected>Choose Your Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Male">Female</option>
+                            <option value="Male">Custom</option>
+
+                        </select>
+                       
+                        <select name="years" class="form-select mt-3">
+                        <option selected>Choose Year Of Birth</option>
+                            <?php 
+                            for ($x=1960; $x<=2024; $x=$x+1){
+                                echo "<option value='".$x."'>".$x."</option>";
+                            }
+                            ?>
+                          
+                            
+                            
+
+                        </select>
+                        <select name="month" class="form-select mt-3">
+                            <option selected> Choose month of birth</option>
+                            <?php 
+                          for ($y=1; $y<=12; $y=$y+1){
+                            echo "<option value='".$y."'> ".$y."</option>";
+                          }
+                            ?>
+                            
+
+                        </select>
+                       
+                            <?php 
+                            $year= date('Y');
+                            $month= array(
+                                1=>'january',
+                                2=>'febuary',
+                                3=>'march',
+                                4=>'april',
+                                5=>'may',
+                                6=>'june',
+                                7=>'july',
+                                8=>'august',
+                                9=>'september',
+                                10=>'october',
+                                11=>'november',
+                                12=>'december',
+                            );
+                            $days= range(1,31);
+                            
+                            ?>
+                           echo '<select name>
+
+                            </select>
+                      
+
+                      
+                     
+                      <button class=" btn btn-primary">Register</button>
+                      <p class="text-small">Dont have an account ?<span class="reg"><a href="index.html"></span>Register</a></p>
+                       
+                      <hr class="text-muted">
+                      <div class="px-btn">
+                        <button class="btn btn-success">Login Here</button>
+                      </div>    
+                      
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
